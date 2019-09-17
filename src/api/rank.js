ca44de866a18c78625e455dfb8c8fccc433b74f3
ARRAY-LIST <1348909544@qq.com>
2019-09-18 00:05:30 +0800

    'rank-https'

--- src/api/rank.js
+++ src/api/rank.js
@@ -2,8 +2,9 @@ import jsonp from 'common/js/jsonp'
 import { commonParams, options } from './config'
 
 export function getTopList () {
-  const url = 'http://c.y.qq.com/v8/fcg-bin/fcg_myqq_toplist.fcg'
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_myqq_toplist.fcg'
   const data = Object.assign({}, commonParams, {
+    uin: 0,
     platform: 'h5',
     needNewCode: 1
   })
